ad76ad658371a77fe44d0f6117b43bf684be7ebd
Udhayanithi96 <63120013+Udhayanithi96@users.noreply.github.com>
2022-07-04 11:28:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var wrapper = document.getElementById('wrapper');
+
+document.addEventListener('click', function (event) {
+  if (!event.target.matches('.list')) return;
+
+  // List view
+  event.preventDefault();
+  wrapper.classList.add('list');
+});
+
+document.addEventListener('click', function (event) {
+  if (!event.target.matches('.grid')) return;
+
+  // List view
+  event.preventDefault();
+  wrapper.classList.remove('list');
+});
